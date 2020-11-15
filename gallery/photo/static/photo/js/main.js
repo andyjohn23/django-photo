06705ee680c963d727455326f3b77263369ea904
andyjohn23 <andrewnick906@gmail.com>
2020-11-16 01:25:02 +0300

    updated files

--- gallery/photo/static/photo/js/main.js
+++ gallery/photo/static/photo/js/main.js
@@ -6,20 +6,3 @@ searchIcon.addEventListener("click", activateSearch);
 function activateSearch() {
   searchBox.classList.toggle("active");
 }
-
-var modal = document.getElementById("myModal");
-
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("photo");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-var span = document.getElementsByClassName("close")[0];
-
-span.onclick = function () {
-  modal.style.display = "none";
-}
